19ed3767a498462d9d0f9de28a5f144450d391dc
Ubuntu <ubuntu@ip-172-31-19-246.ec2.internal>
2024-06-16 18:58:48 +0000

    First quick pass for Lab Week 3

---
+++ text_processor.py
@@ -0,0 +1,16 @@
+def clean_text(input_string):
+	try:
+		assert(isinstance(input_string, str), "Oh no, your input is not a string!"):
+	assert_exception:
+		return "Input must be a string. Please try again, thank you."
+
+	return LOWER(input_string.replace(".", ""))
+
+def tokenize(input_string):
+	tokens = input_string.split(" ")
+
+def count_words(input_string):
+	word_dict = {}
+
+	for i in input string:
+		
