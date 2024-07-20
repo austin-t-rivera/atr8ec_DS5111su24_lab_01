0a6c68c6acab676e68d502c2d7c6135eb7d55cba
Ubuntu <ubuntu@ip-172-31-19-246.ec2.internal>
2024-07-20 02:09:30 +0000

    Lab Part 2: text_processor finalized, makefile updated

--- makefile
+++ makefile
@@ -29,3 +29,8 @@ total_lines:
 total_words:
 	@echo "Total words across all txt files:"
 	@cat *.txt | wc -w
+
+setup:
+	python3 -m venv env
+	.env/bin/pip install --upgrade pip
+	.env/bin/pip install -r requirements.txt
--- text_processor.py
+++ text_processor.py
@@ -1,17 +1,43 @@
-def clean_text(input_string):
-	try:
-		assert(isinstance(input_string, str), "Oh no, your input is not a string!"):
-	assert_exception:
-		return "Input must be a string. Please try again, thank you."
+import string
+import logging
+from collections import Counter
 
-	lower_string =  LOWER(input_string.replace())
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 
+def clean_text(text):
 
-def tokenize(input_string):
-	tokens = input_string.split(" ")
+        assert isinstance(text, str), "Input Error: Text is not a string!"
+        logging.debug(f"Cleaning your text: {text}")
 
-def count_words(input_string):
-	word_dict = {}
+        cleaned_text = text.lower().translate(str.maketrans('', '', string.punctuation))
 
-	for i in input string:
-		
+        assert cleaned_text, "Output Error: Cleaned text cannot be null! Non-punctuation characters are required!"
+        logging.debug(f"Your cleaned text: {cleaned_text}")
+
+        return cleaned_text
+
+def tokenize(text):
+
+        assert isinstance(text, str), "Input Error: Text is not a string!"
+        logging.debug(f"Tokenizing your text: {text}")
+
+        tokens = clean_text(text).split(" ")
+
+        assert isinstance(tokens, list) and all(isinstance(word, str) for word in tokens), "Output Error: Tokens should be a list of strings!"
+        logging.debug(f"Your tokens: {tokens}")
+
+        return tokens
+
+def count_words(text):
+
+        assert isinstance(text, str), "Input Error: Text is not a string!"
+        logging.debug(f"Counting word occurences in the text: {text}")
+
+	word_count = Counter(tokenize(text))
+
+	assert word_counts, "Output Error: Word count dictionary cannot be empty!"
+	assert all(isinstance(word, str) for word in word_counts.keys()), "Output Error: All words must be strings."
+	assert all(isinstance(count, int) for count in word_counts.values()), "Output Error: All counts must be integers."
+	logging.debug(f"Counts for each word: {word_count}")
+
+	return word_count
